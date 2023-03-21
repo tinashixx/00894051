d65f7bc07a2833b59fb21da5ab0d303158751c92
tina <a81612she@gmail.com>
2023-03-21 14:26:57 +0800

    commit

---
+++ srpingjms/src/main/java/com/example/springboot/MessageSender.java
@@ -0,0 +1,21 @@
+package com.example.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageSender {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	@Value("${springjms.myQueue}")
+	private String queue;
+
+	public void send(String message) {
+		System.out.println("Message Sent ===>" + message);
+		jmsTemplate.convertAndSend(queue, message);
+	}
+}
